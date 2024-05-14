9e90a377c10b2d4bedbb48bd27e46581691293f1
Harsh Rajbhar <91482495+HarshRajbhar@users.noreply.github.com>
2024-05-14 15:23:43 +0530

    Dev hrsh (#2)
    * edit and update funtion work properly
    
    * sorting done
    
    * some minor changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,18 +17,18 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   title = 'kendo-angular-app';
-  selected = 'Inbox';
+  selected = 'Grid';
   menuSvg: SVGIcon = menuIcon;
 
   items: Array<DrawerItem> = [
-    { text: 'Table', selected: true },
-    { text: 'Grid' },
+    { text: 'Table', },
+    { text: 'Grid' ,selected: true },
     { text: 'other Components' },
     { text: 'Buttons' },
   ];
   constructor(private router: Router) {}
   ngOnInit(): void {
-    this.selected = 'Table';
+    this.selected = 'Grid';
   }
   onSelect(ev: DrawerSelectEvent): void {
     this.selected = ev.item.text;
